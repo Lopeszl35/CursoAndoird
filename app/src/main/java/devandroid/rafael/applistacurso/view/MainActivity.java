cf3cfb485b2f3062d971d4919e70512e8182b4d1
Lopeszl35 <rafzl490@gmail.com>
2023-03-02 19:26:57 -0300

    Aplicando ID's

--- app/src/main/java/devandroid/rafael/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/rafael/applistacurso/view/MainActivity.java
@@ -4,18 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import devandroid.rafael.applistacurso.R;
 import devandroid.rafael.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Declarando o objeto Pessoa
+    //Declarando a classe Pessoa, e o objeto pessoa
     Pessoa pessoa;
-    //Declarando novamente o objeto pessoa
+    //Declarando novamente a classe Pessoa, e o objeto pessoa
     Pessoa outraPessoa;
 
     String dadosPessoa;
+
+    //Classe, Objeto
+    EditText editPrimeiroNome;
+    EditText editSobreNome;
+    EditText editNomeCurso;
+    EditText editTelefoneContato;
+    Button btnlimpar;
+    Button btnsalvar;
+    Button btnfinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +48,21 @@ public class MainActivity extends AppCompatActivity {
         outraPessoa.setCursoDesejado("ADS");
         outraPessoa.setTelefoneContato("18981218829");
 
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobreNome = findViewById(R.id.editSobreNome);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+
+        btnlimpar = findViewById(R.id.btnlimpar);
+        btnfinalizar = findViewById(R.id.btnfinalizar);
+        btnsalvar = findViewById(R.id.btnsalvar);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobreNome.setText(pessoa.getSobreNome());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+        editNomeCurso.setText(pessoa.getCursoDesejado());
+
+        /*
         dadosPessoa = "Primeiro nome: ";
         dadosPessoa += pessoa.getPrimeiroNome();//o atributo 'get' pega o atributo relacionado ao dado
         dadosPessoa += " Sobrenome: ";
@@ -45,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         dadosPessoa += pessoa.getCursoDesejado();
         dadosPessoa += " Telefone de Contato: ";
         dadosPessoa += pessoa.getTelefoneContato();
+         */
 
         Log.i("POOAndroid", pessoa.toString());
         Log.i("POOAndroid", outraPessoa.toString());
