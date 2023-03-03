f04ca61632a83649d9483bba33c1e81ee2d80118
Lopeszl35 <rafzl490@gmail.com>
2023-03-02 22:16:35 -0300

    Metodo Salvar

--- app/src/main/java/devandroid/rafael/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/rafael/applistacurso/view/MainActivity.java
@@ -10,16 +10,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import devandroid.rafael.applistacurso.R;
+import devandroid.rafael.applistacurso.controller.PessoaController;
 import devandroid.rafael.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    PessoaController controller;
 
     //Declarando a classe Pessoa, e o objeto pessoa
     Pessoa pessoa;
     //Declarando novamente a classe Pessoa, e o objeto pessoa
     Pessoa outraPessoa;
-
-    String dadosPessoa;
 
     //Classe, Objeto
     EditText editPrimeiroNome;
@@ -35,14 +36,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pessoa = new Pessoa();
+        //Conectando a Classe MainActivity a PessoaController
+        controller = new PessoaController();
+        controller.toString();
 
-        //Atribuindo atributos ao objeto
-        //Conforme o seu modelo, feito na classe Pessoa
-        //pessoa.setPrimeiroNome("Rafael");
-        //pessoa.setSobreNome("Amaro Lopes");
-        //pessoa.setCursoDesejado("Android");
-        //pessoa.setTelefoneContato("18-991729160");
+        //Conectando o objeto pessoa a classe Pessoa
+        pessoa = new Pessoa();
 
         outraPessoa = new Pessoa();
         outraPessoa.setPrimeiroNome("Lucimara");
@@ -93,19 +92,11 @@ public class MainActivity extends AppCompatActivity {
                 pessoa.setCursoDesejado(editNomeCurso.getText().toString());
 
                 Toast.makeText(MainActivity.this, "Salvo" + pessoa.toString(), Toast.LENGTH_LONG).show();
+
+                //Controladora recebe o metodo salvar, com os atributos da clase pessoa
+                controller.salvar(pessoa);
             }
         });
-
-        /*
-        dadosPessoa = "Primeiro nome: ";
-        dadosPessoa += pessoa.getPrimeiroNome();//o atributo 'get' pega o atributo relacionado ao dado
-        dadosPessoa += " Sobrenome: ";
-        dadosPessoa += pessoa.getSobreNome();
-        dadosPessoa += " Curso Desejado: ";
-        dadosPessoa += pessoa.getCursoDesejado();
-        dadosPessoa += " Telefone de Contato: ";
-        dadosPessoa += pessoa.getTelefoneContato();
-         */
 
         Log.i("POOAndroid", pessoa.toString());
         Log.i("POOAndroid", outraPessoa.toString());
